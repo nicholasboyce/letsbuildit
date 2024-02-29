c932a662638c7bdc7bd9bf134403c292673e8b99
nicholasboyce <nicholasboyce@protonmail.com>
2024-02-28 19:54:13 -0500

    Adjusted project structure to accomodate frontend and backend

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: resolve(__dirname, 'client'),
+  build: {
+    emptyOutDir: true,
+    outDir: resolve(__dirname, 'src/main/resources/static'),
+    rollupOptions: {
+      input: resolve(__dirname, 'client/index.html')
+    }
+  },
+  plugins: [react()],
+})
