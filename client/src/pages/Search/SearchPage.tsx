78b1b0e6795bf50d0723e6e4bccf8d4a47a0ac9b
nicholasboyce <nicholasboyce@protonmail.com>
2024-07-30 16:12:21 -0400

    Added search form without styling using native HTML elements.

--- client/src/pages/Search/SearchPage.tsx
+++ client/src/pages/Search/SearchPage.tsx
@@ -7,6 +7,20 @@ const SearchPage = () => {
             <div className={`${styles["page"]}`}>
                 <PageHeader />
                 <main className={styles["main-landmark"]}>
+                    <h1 id='search-label'>Find your new coding community!</h1>
+                    <search aria-labelledby='search-label'>
+                        <form id='main-content' aria-label='Project search'>
+                            <label htmlFor="project-search">Project Title</label>
+                            <input type="search" name="project-search" id="project-search" />
+                            <label htmlFor="difficulty">Difficulty</label>
+                            <select name="difficulty" id="difficulty"></select>
+                            <label htmlFor="language">Language</label>
+                            <select name="language" id="language"></select>
+                            <label htmlFor="timezone">Timezone</label>
+                            <select name="timezone" id="timezone"></select>
+                            <button type='submit'>Search Projects</button>
+                        </form>
+                    </search>
                 </main>
             </div>
         </div>
