97a72c2950c547d76f92de7299cbba47ae6c6312
nicholasboyce <nicholasboyce@protonmail.com>
2024-08-21 20:01:47 -0400

    Added usersRouter to main router and expanded GET endpoint test.

--- server/router.ts
+++ server/router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { csrfSync } from 'csrf-sync';
 import { logger } from './utils/logger';
+import { usersRouter } from './routes/users';
 import { csrfRouter } from './routes/csrf';
 import { authRouter } from './routes/auth';
 
@@ -10,5 +11,6 @@ const Router = express.Router();
 Router.use('/csrf', csrfRouter);
 Router.use(csrfSynchronisedProtection);
 Router.use('/auth', authRouter);
+Router.use('/users', usersRouter);
 
 export { Router };
