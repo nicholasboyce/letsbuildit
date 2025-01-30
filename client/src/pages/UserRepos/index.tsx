8abf732b72d87feb7bcac8aecb2fa486b2dcff10
nicholasboyce <gh@nicholasboyce.dev>
2025-01-30 11:22:31 -0500

    Added wrapPromise utility function.
    Co-authored-by: plocket <52798256+plocket@users.noreply.github.com>

--- client/src/pages/UserRepos/index.tsx
+++ client/src/pages/UserRepos/index.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { wrapPromise } from "../../utils/wrapPromise";
 
 export const UserRepos = () => {
     const { name } = useParams();
+    const [repos, setRepos] = useState([]);
+
     useEffect(() => {
         fetch(`https://api.github.com/users/${name}/repos`)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => {
+                console.log(data);
+                setRepos(data);
+            });
     }, [name]);
+
+    console.log(repos);
     return (<></>);
 };
