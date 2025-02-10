f89be5694e5ae2cd2c11c0003c5ac909800f7cc6
nicholasboyce <gh@nicholasboyce.dev>
2025-02-10 18:32:21 -0500

    Remade page routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 import './App.css';
 import { Home, SearchPage, Feed, FullPost, UserRepos } from './pages';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Outlet } from 'react-router-dom';
 
 function App() {
 
@@ -9,13 +9,23 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/search' element={<SearchPage />} />
-      <Route path='/feed' element={<Feed />} />
-      <Route path='/posts/:id' element={<FullPost />} />
-      <Route path='/users/:name/repos' element={<UserRepos />} />
+      <Route path='/*' element={<><h1>Octopod</h1><Outlet /></>}>
+        <Route path='posts'>
+          <Route index element={<Feed/>} />
+          <Route path='me' element={<p>My page!</p>}/>
+          <Route path=':name' element={<p>Someone else's page!</p>} />
+          <Route path=':name/:post' element={<p>The specific post of someone else!</p>} />
+        </Route>
+        <Route path='*' element={<h1>Sorry! You're Lost...</h1>} />
+      </Route>
     </Routes>
     </>
   )
 }
 
 export default App;
+
+      {/* <Route path='/search' element={<SearchPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/posts/:id' element={<FullPost />} />
+      <Route path='/users/:name/repos' element={<UserRepos />} /> */}
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -3,25 +3,17 @@ import styles from './Home.module.css';
 import HeroImage from '../../assets/mobile-testing-isometric.png';
 
 export const Home = () => {
+    const handleClick = () => {};
     return (
-        <div className={`${styles["homepage-wrapper"]}`}>
-            <div className={`${styles["homepage"]}`}>
-                <PageHeader />
-                <main className={styles["main-landmark"]}>
-                    <img src={HeroImage} className={styles["hero-image"]} alt='' />
-                    <section aria-label='Hero' className={styles["hero-section"]}>
-                        <div className={styles["hero-section-wrapper"]} id='main-content'>
-                            <h1 className={styles["hero-heading"]}>Find your new coding community!</h1>
-                            <p className={styles["hero-text"]}>
-                                Newbie looking for your first collab?<br />
-                                Hobbyist who wants to work with friends?<br />
-                                10x Engineer in need of your dream team?<br />
-                            </p>
-                            <Button />
-                        </div>
-                    </section>
-                </main>
-            </div>
-        </div>
-    )
+        <>
+            <h1>Octopod</h1>
+            <main>
+                <h2>See what your fellow Recursers have been up to!</h2>
+                <button>Log In</button>
+                <p>At RC, everyone is always making something interesting- it can be hard to keep up!
+                Octopod is meant to help make it easier to find cool projects in the past and present, and make it easier for others to find you too!</p>
+            </main>
+        </>
+
+    );
 };
