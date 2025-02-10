13b511da045620c35773c5cc8376d2402c4c2f04
nicholasboyce <gh@nicholasboyce.dev>
2025-02-10 15:40:29 -0500

    Listitems of repos

--- client/src/pages/UserRepos/Repo.tsx
+++ client/src/pages/UserRepos/Repo.tsx
@@ -1,5 +1,4 @@
 import { use } from "react";
-import { ProjectPost } from "../../../types";
 
 interface Repo {
     name: string
@@ -9,20 +8,39 @@ interface Repo {
     created_at: Date
 }
 
+interface RepoProps {
+    repo: Repo
+}
+
 // const RepoSchema = z.object({
 //     name: z.string(),
 //     author: z.string(),
 //     description: z.string(),
-    
 // });
+
+const Repo = ({ repo }: RepoProps) => {
+    const postDate = new Date(repo.created_at);
+    return (
+        <li>
+            <article>
+                <h2><a href={repo.html_url}>{repo.name}</a></h2>
+                <p>{repo.description ? repo.description : "Description not found."}</p>
+                <p>Posted: <time datetime={repo.created_at}>{postDate.toUTCString()}</time></p>
+            </article>
+        </li>
+    )
+};
 
 export const RepoList = ({ resourcePromise }) => {
     // const result = resource.result.read();
-    const data : ProjectPost = use(resourcePromise);
+    const unsortedData = use(resourcePromise);
+    const data = unsortedData.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
     return (
         <ul>
-            {data.map((repo) => <li key={repo.full_name}>{repo.name}</li>)}
+            {data.map((repo) => {
+                return <Repo key={repo.full_name} repo={repo} />
+            })}
         </ul>
     );
 };
