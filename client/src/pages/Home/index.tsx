951213243694a048444dadb612b8dbc1fe776c7e
nicholasboyce <gh@nicholasboyce.dev>
2025-03-04 22:58:37 -0500

    Updated and styled unlogged in homepage

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -6,9 +6,11 @@ export const Home = () => {
     const handleClick = () => {};
     return (
         <>
-            <h1>Octopod</h1>
+            <header>
+                <h1>Octopod 🐙</h1>
+            </header>
             <main>
-                <h2>See what your fellow Recursers have been up to!</h2>
+                <h2 className={styles["hero-heading"]}>See what your fellow Recursers have been up to!</h2>
                 <button>Log In</button>
                 <p>At RC, everyone is always making something interesting- it can be hard to keep up!
                 Octopod is meant to help make it easier to find cool projects in the past and present, and make it easier for others to find you too!</p>
